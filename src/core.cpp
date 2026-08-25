b4eafcdf6426f5370344311c864a1f582f3438c2
GrazerDev <everythingistaken325@gmail.com>
2026-08-25 08:30:38 +0530

    Update core.cpp

--- src/core.cpp
+++ src/core.cpp
@@ -3,21 +3,30 @@
 #include <emscripten/emscripten.h>
 
 extern "C" {
-    // The "Forge" challenge: Calculate primes up to N using Sieve of Eratosthenes
-    EMSCRIPTEN_KEEPALIVE
-    int run_challenge(int n) {
-        int count = 0;
-        std::vector<bool> is_prime(n + 1, true);
-        is_prime[0] = is_prime[1] = false;
-        for (int p = 2; p * p <= n; p++) {
-            if (is_prime[p]) {
-                for (int i = p * p; i <= n; i += p)
-                    is_prime[i] = false;
-            }
+
+// This is the function people will try to optimize.
+// The goal: Find how many prime numbers exist up to 'n' as fast as possible.
+EMSCRIPTEN_KEEPALIVE
+int run_challenge(int n) {
+    if (n < 2) return 0;
+
+    // Standard Sieve of Eratosthenes
+    std::vector<bool> is_prime(n + 1, true);
+    is_prime[0] = is_prime[1] = false;
+
+    for (int p = 2; p * p <= n; p++) {
+        if (is_prime[p]) {
+            for (int i = p * p; i <= n; i += p)
+                is_prime[i] = false;
         }
-        for (int p = 2; p <= n; p++) {
-            if (is_prime[p]) count++;
-        }
-        return count;
     }
+
+    int count = 0;
+    for (int p = 2; p <= n; p++) {
+        if (is_prime[p]) count++;
+    }
+
+    return count;
+}
+
 }
